9668378663d99db915772dddc17a91888b30280a
Ouasmine <m.amine.osm@gmail.com>
2016-05-10 13:25:41 +0200

    Add elasticSearch not yet completed

---
+++ ElasticSearchService/src/main/java/com/pixalione/mailliageinterne/elasticsearch/Model/KeyWord.java
@@ -0,0 +1,7 @@
+package com.pixalione.mailliageinterne.elasticsearch.Model;
+
+/**
+ * Created by OUASMINE Mohammed Amine on 10/05/2016.
+ */
+public class KeyWord {
+}
---
+++ ElasticSearchService/src/main/java/com/pixalione/mailliageinterne/elasticsearch/Repository/KeyWordRepository.java
@@ -0,0 +1,7 @@
+package com.pixalione.mailliageinterne.elasticsearch.Repository;
+
+/**
+ * Created by OUASMINE Mohammed Amine on 10/05/2016.
+ */
+public class KeyWordRepository {
+}
